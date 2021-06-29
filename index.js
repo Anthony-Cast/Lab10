03e00048b0e243e1b3989742f0abeb92d214fff4
anthonycast <acastanedap@pucp.edu.pe>
2021-06-29 15:46:03 -0500

    ordenamiento

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ let conn = mysql.createConnection({
     database: 'lab10_employees'
 });
 
+
 conn.connect(function(err){
     if(err) throw err;
     console.log("Conexión exitosa a base de datos");
 });
 
+//Pregunta 1
 app.get("/empleados/get",function (req, res){
     conn.query("SELECT EmployeeID, LastName, FirstName, Title FROM lab10_employees.employees", function (err,results){
         if (err) throw err;
@@ -23,6 +25,7 @@ app.get("/empleados/get",function (req, res){
     });
 });
 
+//Pregunta 2
 app.get("/empleados/getManagerEmployees/:id",function (req, res){
     let ReportsTo = req.params.id;
     let sql = "SELECT EmployeeID, LastName, FirstName, Title FROM lab10_employees.employees where ReportsTo = ?";
@@ -33,6 +36,18 @@ app.get("/empleados/getManagerEmployees/:id",function (req, res){
     });
 });
 
+//Pregunta 3
+app.get("/empleados/getByTitle/:title",function (req, res) {
+    let Title = req.params.title;
+    let sql="SELECT EmployeeID, LastName, FirstName, Title FROM lab10_employees.employees where Title =?";
+    let params=[Title];
+    conn.query(sql,params,function (err,results){
+        if(err) throw err;
+        res.json(results);
+    })
+});
+
+//Pregunta 4
 app.post("/empleados/update",bodyParser.urlencoded({extended:true}), function (req,res){
     var id = req.body.Id;
     var address = req.body.Address;
@@ -50,32 +65,23 @@ app.post("/empleados/update",bodyParser.urlencoded({extended:true}), function (r
         var params =[email,address,id];
     }
     conn.query(sql,params,function (err,results)
-        {
-            if (err){
-                res.json({
-                   "status":"error",
-                   "message":err.sqlMessage
-                });
-            }
-            else{
-                res.json({
-                    "status":"ok",
-                    "message":"Employee updated"
-                });
-            }
-        });
+    {
+        if (err){
+            res.json({
+                "status":"error",
+                "message":err.sqlMessage
+            });
+        }
+        else{
+            res.json({
+                "status":"ok",
+                "message":"Employee updated"
+            });
+        }
+    });
 });
 
-app.get("/empleados/getByTitle/:title",function (req, res) {
-    let Title = req.params.title;
-    let sql="SELECT EmployeeID, LastName, FirstName, Title FROM lab10_employees.employees where Title =?";
-    let params=[Title];
-    conn.query(sql,params,function (err,results){
-        if(err) throw err;
-        res.json(results);
-    })
-});
-
+//Puerto
 app.listen(3000,function (){
    console.log("Servidor levantado");
 });
